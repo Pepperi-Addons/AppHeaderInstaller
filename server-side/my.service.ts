126c5010816de803183c4d038cc43694b55267f6
bennyts <benny.t@pepperi.com>
2023-10-29 11:57:55 +0200

    change while condition

--- server-side/my.service.ts
+++ server-side/my.service.ts
@@ -1,4 +1,4 @@
-import { PapiClient} from '@pepperi-addons/papi-sdk'
+import { AddonAPIAsyncResult, PapiClient} from '@pepperi-addons/papi-sdk'
 import { Client} from '@pepperi-addons/debug-server';
 import Semver from "semver";
 
@@ -11,20 +11,20 @@ class MyService {
     papiClient: PapiClient;
 
     addOnsToInstall = [
-        {name: 'Pfs', uuid: '00000000-0000-0000-0000-0000000f11e5', ver: '1.3.8', phased: true, release: true},
+        {name: 'Pfs', uuid: '00000000-0000-0000-0000-0000000f11e5', ver: '1.3.8', phased: false, release: false},
         {name: 'System Health', uuid: 'f8b9fa6f-aa4d-4c8d-a78c-75aabc03c8b3', ver: '1.1.81', phased: true, release: true},
         {name: 'Nebula', uuid: '00000000-0000-0000-0000-000000006a91', ver: '1.0.112', phased: true, release: true},
         {name: 'Sync', uuid: '5122dc6d-745b-4f46-bb8e-bd25225d350a', ver: '0.7.84', phased: false, release: false},
         {name: 'Dimx', uuid: '44c97115-6d14-4626-91dc-83f176e9a0fc', ver: '1.0.13', phased: true, release: true},
-        {name: 'Cpi node', uuid: 'bb6ee826-1c6b-4a11-9758-40a46acb69c5', ver: '1.5.4', phased: true, release: true},
+        {name: 'Cpi node', uuid: 'bb6ee826-1c6b-4a11-9758-40a46acb69c5', ver: '1.5.17', phased: false, release: false},
         {name: 'Cpi node data', uuid: 'd6b06ad0-a2c1-4f15-bebb-83ecc4dca74b', ver: '0.6.17', phased: true, release: true},
         {name: 'Papi', uuid: '00000000-0000-0000-0000-000000000a91', ver: '9.6.24', phased: true, release: true},
         {name: 'cpapi', uuid: '00000000-0000-0000-0000-000000abcdef', ver: '9.6.31', phased: true, release: true},
         {name: 'Core', uuid: '00000000-0000-0000-0000-00000000c07e', ver: '1.0.7', phased: true, release: true},
         {name: 'Core resources', uuid: 'fc5a5974-3b30-4430-8feb-7d5b9699bc9f', ver: '1.0.23', phased: true, release: true},
         {name: 'Configuration', uuid: '84c999c3-84b7-454e-9a86-71b7abc96554', ver: '0.6.102', phased: false, release: true},
-        {name: 'Flows', uuid: 'dc8c5ca7-3fcc-4285-b790-349c7f3908bd', ver: '0.6.17', phased: false, release: false},
-        {name: 'Cpas', uuid: '00000000-0000-0000-0000-0000003eba91', ver: '17.20.9', phased: true, release: true},
+        {name: 'Flows', uuid: 'dc8c5ca7-3fcc-4285-b790-349c7f3908bd', ver: '0.6.17', phased: false, release: true},
+        {name: 'Cpas', uuid: '00000000-0000-0000-0000-0000003eba91', ver: '17.30.1', phased: false, release: false},
        // {name: 'Local storage', uuid: '5130126c-1c2a-446f-9d08-754269fdaa72', ver: '1.0.1', phased: false, release: false},
         {name: 'Slugs', uuid: '4ba5d6f9-6642-4817-af67-c79b68c96977', ver: '1.2.23', phased: true, release: true},
         {name: 'Assets Manager ', uuid: 'ad909780-0c23-401e-8e8e-f514cc4f6aa2', ver: '1.2.2', phased: false, release: false},
@@ -80,10 +80,10 @@ class MyService {
     getInstalledAddons(): Promise<any[]> {
         return this.papiClient.addons.installedAddons.iter({}).toArray();
     }
-    async installAddonRecursive(result: any, addonName: string){
+    async installAddonRecursive(result: AddonAPIAsyncResult, addonName: string){
         let statusResponse = await this.papiClient.get(`/audit_logs/${result.ExecutionUUID}`);
 
-        while (statusResponse === undefined || statusResponse.Status.Name === 'New' || statusResponse.Status.Name === 'InProgress' || statusResponse.Status.Name === 'Started') {
+        while (!statusResponse || statusResponse.Status.Name === 'New' || statusResponse.Status.Name === 'InProgress' || statusResponse.Status.Name === 'Started') {
             await sleep(1000);
             statusResponse = await this.papiClient.get(`/audit_logs/${result.ExecutionUUID}`);
         }
