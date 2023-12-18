da4d7fcd1ed49817cf7ba656213d55e41a99e73f
bennyts <benny.t@pepperi.com>
2023-12-18 14:04:52 +0200

    no message

---
+++ server-side/addonsList.ts
@@ -0,0 +1,40 @@
+//00000000-0000-0000-0000-00000000c07e      Core                                            1.1.3 done
+//00000000-0000-0000-0000-0000000f11e5 	    Pfs                                             1.3.86 done
+//0eb2627d-2bc2-4d73-9ac6-d5b191cb59a2	    Buttons                 clientZip = true        0.0.53 done
+//16d2052b-55b7-43b5-9d3b-a2f9d9950d59	    SlideShow2              clientZip = true        2.0.34 done
+//194772e8-1c45-48ee-9401-bea82f0d7c65	    Gallery2                clientZip = true        2.0.14 done
+//3864cd44-b388-41c5-8af9-ec200f72b3f3	    RichText                clientZip = true        1.0.24 done
+//44c97115-6d14-4626-91dc-83f176e9a0fc	    Dimx                                            2.0.5 done  
+//4ba5d6f9-6642-4817-af67-c79b68c96977	    Slugs                                           1.2.25 done
+//50062e0c-9967-4ed4-9102-f2bc50602d41	    Pages                   clientZip = true        2.0.116 done
+//5122dc6d-745b-4f46-bb8e-bd25225d350a	    Sync                                            1.0.46 done  
+//58054aa9-5667-4fbf-9b9a-f9c94cd00906	    Banner                  clientZip = true        0.9.21 done
+//84c999c3-84b7-454e-9a86-71b7abc96554	    Configuration                                   0.7.2 done  
+//95501678-6687-4fb3-92ab-1155f47f839e	    Themes                                          2.1.8 done
+//9bc8af38-dd67-4d33-beb0-7d6b39a6e98d	    Application Header                              0.5.45 done
+//a446f8ae-3787-497b-9a92-82e571bdf48e	    logic blocks                                    1.0.16 done
+//ad909780-0c23-401e-8e8e-f514cc4f6aa2	    Assets                                          1.2.15 done
+//bb6ee826-1c6b-4a11-9758-40a46acb69c5	    cpi node                                        1.5.50   done  
+//d00398a6-3b65-47a0-b02c-08f5de960740	    filter block            clientZip = true        0.5.30 done
+//d6b06ad0-a2c1-4f15-bebb-83ecc4dca74b	    cpi node data                                   0.6.19 done  
+//dc8c5ca7-3fcc-4285-b790-349c7f3908bd	    flows                                           0.6.22 done  
+//fc5a5974-3b30-4430-8feb-7d5b9699bc9f	    core resources                                  1.1.62 done
+//00000000-0000-0000-0000-00000da1a109	    Audit Log                                       1.0.60 done  
+//00000000-0000-0000-0000-0da1a0de41e5	    Data Visualization                              1.1.40 done
+//0e2ae61b-a26a-4c26-81fe-13bdd2e4aaa3	    Resource List           clientZip = true
+//122c0e9d-c240-4865-b446-f37ece866c22	    User Defined Collections                        0.8.36 done 
+//1238582e-9b32-4d21-9567-4e17379f41bb	    UOM                                             1.4.4  done 
+//2b462e9e-16b5-4e7a-b1e6-9e2bfb61db7e	    VisitFlow               clientZip = true        0.6.38 done
+//316afc44-af38-4354-ac4c-22011cb0ea84	    atd-events                                      0.5.18 done  
+//41011fbf-debf-40d8-8990-767738b8af03	    Journey Tracker                                 0.3.18 done   
+//5adbc9e0-ed1d-4b2d-98e9-9c50891812ea	    Gallery1                clientZip = true
+//70d671e5-6cc4-40f8-b3c5-a2c09079e5ea	    page-parameters-setter                          1.0.3
+//95025423-9096-4a4f-a8cd-d0a17548e42e	    Push Notifications      clientZip = true
+//9f3b727c-e88c-4311-8ec4-3857bc8621f3	    Scripts                                         0.6.36 done            
+//adb3c829-110c-4706-9168-40fba9c0eb52	    Pricing
+//cbbc42ca-0f20-4ac8-b4c6-8f87ba7c16ad	    User Defined Events                             0.7.4  done   
+//cf17b569-1af4-45a9-aac5-99f23cae45d8	    Survey Builder          clientZip = true        0.8.27  done
+//d2e046c0-8e2d-4cf6-979e-a365fca4a095	    transaction-events                              0.0.119 done  
+//dd0a85ea-7ef0-4bc1-b14f-959e0372877a	    Survey                                          0.6.2   done
+//df90dba6-e7cc-477b-95cf-2c70114e44e0	    Generic Resource                                0.6.4   done  
+//f93658be-17b6-4c92-9df3-4e6c7151e038	    SlideShow1              clientZip = true
